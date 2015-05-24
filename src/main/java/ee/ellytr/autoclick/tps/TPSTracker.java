e1ac658f6802729011e57fc7e8f421e7c23e3c42
iEli2tyree011 <connorjayr@gmail.com>
2015-05-23 23:35:16 -0400

    Fix a needless ArithmeticException with TPS tracking

--- src/main/java/ee/ellytr/autoclick/tps/TPSTracker.java
+++ src/main/java/ee/ellytr/autoclick/tps/TPSTracker.java
@@ -2,13 +2,12 @@ package ee.ellytr.autoclick.tps;
 
 public class TPSTracker {
 
-    private static double lastTick = System.nanoTime();
+    private static double lastTick = -1;
     private static double tps = 0;
 
     public static void tick() {
-        try {
+        if (lastTick != -1) {
             tps = 1 / ((System.nanoTime() - lastTick) / 1000000000.0);
-        } catch (ArithmeticException e) {
         }
         lastTick = System.nanoTime();
     }
