287f8d077b78720e60d619aa326ed42f07c9912c
iEli2tyree011 <connorjayr@gmail.com>
2015-05-24 15:01:10 -0400

    Fix TPS warning messages being sent too many times

--- src/main/java/ee/ellytr/autoclick/tps/TPSRunnable.java
+++ src/main/java/ee/ellytr/autoclick/tps/TPSRunnable.java
@@ -12,10 +12,8 @@ public class TPSRunnable implements Runnable {
         for (Player player : Bukkit.getOnlinePlayers()) {
             double tps = Math.round(TPSTracker.getTPS() * 100.0) / 100.0;
             if (tps <= EllyCheat.getInstance().getConfig().getInt("tps-warning")) {
-                for (Player player2 : Bukkit.getOnlinePlayers()) {
-                    if (player2.hasPermission("ellycheat.cps.warning")) {
-                        player2.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "!" + ChatColor.GRAY + "] The server is currently running at " + ChatColor.GOLD + tps + ChatColor.GRAY + " TPS!");
-                    }
+                if (player.hasPermission("ellycheat.cps.warning")) {
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "!" + ChatColor.GRAY + "] The server is currently running at " + ChatColor.GOLD + tps + ChatColor.GRAY + " TPS!");
                 }
                 if (EllyCheat.getInstance().getConfig().getBoolean("log-warnings")) {
                     Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "!" + ChatColor.GRAY + "] The server is currently running at " + ChatColor.GOLD + tps + ChatColor.GRAY + " TPS!");
